c0a1b3a28dc20ae3a73b76bbb2b594b117ff5804
DiegoPes-04 <diegopesillo6@gmail.com>
2024-09-10 13:51:46 -0500

    Create Empleadito.java

---
+++ Modulo1/Preparacion1_corte/empleados/Out_user/Empleadito.java
@@ -0,0 +1,50 @@
+public class Empleadito 
+{
+    private int id;
+    private String nombre;
+    private float  salarioBase;
+
+    public Empleadito (int id, String nombre , float salarioBase)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+    }
+
+    public int getId ()
+    {
+        return id;
+    }
+
+    public String getNombre ()
+    {
+        return nombre;
+    }
+
+    public float  getSalarioBase()
+    {
+        return salarioBase;
+    }
+
+    public void setId (int id)
+    {
+        this.id = id;
+    }
+
+    public void setNombre (String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public void setSalarioBase (float  salarioBase)
+    {
+        this.salarioBase = salarioBase;
+    }
+    
+    @Override
+    public String toString ()
+    {
+        return String.format("Id: %d, Nombre: %s, Salario base: %.2f" , id , nombre , salarioBase);
+    }
+}
+
